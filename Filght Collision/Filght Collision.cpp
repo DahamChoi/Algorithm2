2f09ce6c260c3bdcc6de65963d17f1a0c1359742
DahamChoi <rorigu32@gmail.com>
2021-06-10 21:59:43 +0900

    Filight Collision

---
+++ Filght Collision/Filght Collision.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+struct drone {
+    long double m;
+    int index;
+};
+
+struct compare {
+    bool operator()(const pair<long double, pair<int, int>>& left, const pair<long double, pair<int, int>>& right) {
+        return left.first > right.first;
+    }
+};
+
+long double get_meet_point(long double m1, long double b1, long double m2, long double b2) {
+    if (m1 == m2) {
+        return -1.0; //{ -1.0, -1.0 };
+    }
+
+    long double x = { (b2 - b1) / (m1 - m2) };
+//    long double y = { (m1 * (b2 - b1) / (m1 - m2)) + b1 };
+    return x; //{ x, y };
+}
+
+int main() {
+    int N;  scanf("%d", &N);
+    map<long double, drone> drones;
+    for (int i = 0; i < N; i++) {
+        long double x, v;
+        scanf("%Lf %Lf", &x, &v);
+        drones[x] = { v, i + 1 };
+    }
+
+    priority_queue<
+        pair<long double, pair<int,int>>,
+        vector<pair<long double, pair<int,int>>>, compare> pq;
+
+    for (auto it = drones.begin(); it != drones.end(); it++) {
+        auto& current = (it);
+        if (current->second.m > 0) {
+            auto next = (++it);    --it;
+            if (next != drones.end()) {
+                long double x = get_meet_point(current->second.m, current->first, next->second.m, next->first);
+                if (x > 0) {
+                    pq.push({ x, {current->first, next->first } });
+                }
+            }
+        }
+        else if (current->second.m < 0 && it != drones.begin()) {
+            auto prev = (--it);    ++it;
+            long double x = get_meet_point(current->second.m, current->first, prev->second.m, prev->first);
+            if (x > 0) {
+                pq.push({ x, { prev->first, current->first }});
+            }
+        }
+    }
+
+    while (!pq.empty()) {
+        pair<long double, pair<int, int>> top = pq.top();
+        pq.pop();
+
+        if (drones.find(top.second.first) != drones.end() &&
+            drones.find(top.second.second) != drones.end()) {
+            auto prev = drones.find(top.second.first);
+            --prev;
+            auto next = drones.find(top.second.second);
+            ++next;
+            if (prev != drones.end() && next != drones.end()) {
+                long double x = get_meet_point(prev->second.m, prev->first, next->second.m, next->first);
+                if (x > 0) {
+                    pq.push({ x, {prev->first, next->first} });
+                }
+            }
+
+            drones.erase(top.second.first);
+            drones.erase(top.second.second);
+        }
+    }
+
+    vector<pair<long double, drone>> v_drones(drones.begin(), drones.end());
+    sort(v_drones.begin(), v_drones.end(), [](const pair<long double, drone>& left, const pair<long double, drone>& right) {
+        return left.second.index < right.second.index;
+        });
+
+    printf("%d\n", drones.size());
+    for (auto& it : drones) {
+        printf("%d ", it.second.index);
+    }
+
+    return 0;
+}
---
+++ Island Tour/Island Tour.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct person {
+	int pos, spend_time, start_pos, index;
+};
+
+bool simuration(vector<vector<int>>& spend_time, vector<int>& distance, int s1, int s2, int s3) {
+	int time = 0;
+	vector<person> persons = {
+		{ s1 * 2, 0, s1 * 2, 0 },
+		{ s2 * 2, 0, s2 * 2, 1 },
+		{ s3 * 2, 0, s3 * 2, 2 }
+	};
+
+	while (!persons.empty()) {
+		for (int i = 0; i < persons.size(); i++) {
+			if (persons[i].pos % 2 == 0) {
+				if (++persons[i].spend_time >= spend_time[persons[i].index][persons[i].pos / 2]) {
+					persons[i].spend_time = 0;
+					++persons[i].pos;
+				}
+			}
+			else {
+				if (++persons[i].spend_time >= distance[persons[i].pos / 2]) {
+					persons[i].spend_time = 0;
+					persons[i].pos = persons[i].pos >= distance.size() * 2 - 1 ? 0 : persons[i].pos + 1;
+					if (persons[i].pos == persons[i].start_pos) {
+						persons.erase(persons.begin() + i);
+						--i;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < persons.size(); i++) {
+			for (int j = i + 1; j < persons.size(); j++) {
+				if (persons[i].pos % 2 == 0 && persons[j].pos % 2 == 0 && persons[i].pos == persons[j].pos) {
+					return false;
+				}
+			}
+		}
+	}
+
+	return true;
+}
+
+bool simuration_2(vector<vector<int>>& spend_time, vector<int>& distance, int s1, int s2) {
+	int time = 0;
+	vector<person> persons = {
+		{ s1 * 2, 0, s1 * 2, 0 },
+		{ s2 * 2, 0, s2 * 2, 1 }
+	};
+
+	while (!persons.empty()) {
+		for (int i = 0; i < persons.size(); i++) {
+			if (persons[i].pos % 2 == 0) {
+				if (++persons[i].spend_time >= spend_time[i][persons[i].pos / 2]) {
+					persons[i].spend_time = 0;
+					++persons[i].pos;
+				}
+			}
+			else {
+				if (++persons[i].spend_time >= distance[persons[i].pos / 2]) {
+					persons[i].spend_time = 0;
+					persons[i].pos = persons[i].pos >= distance.size() * 2 - 1 ? 0 : persons[i].pos + 1;
+					if (persons[i].pos == persons[i].start_pos) {
+						return true;
+					}
+				}
+			}
+		}
+
+		if (persons[0].pos % 2 == 0 && persons[1].pos % 2 == 0 && persons[0].pos == persons[1].pos) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int main() {
+	ios::sync_with_stdio(false);	cin.tie(NULL);
+	int N;	cin >> N;
+
+	vector<int> distance(N);
+	for (int i = 0; i < N; i++) {
+		cin >> distance[i];
+	}
+
+	vector<vector<int>> spend_time(3, vector<int>(N));
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> spend_time[i][j];
+		}
+	}
+
+	vector<bool> cnd(N);
+	for (int i = 0; i < 2; i++) {
+		cnd[i] = true;
+	}
+
+	vector<vector<bool>> memory(N, vector<bool>(N));
+
+	do {
+		vector<int> pos;
+		for (int i = 0; i < N; i++) {
+			if (cnd[i]) {
+				pos.push_back(i);
+			}
+		}
+
+		if (simuration_2(spend_time, distance, pos[0], pos[1])) {
+			memory[pos[0]][pos[1]] = true;
+		}
+	} while (prev_permutation(cnd.begin(), cnd.end()));
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (memory[i][j]) {
+				for (int k = 0; k < N; k++) {
+					if (k == i || k == j) {
+						continue;
+					}
+
+					if (simuration(spend_time, distance, i, j, k)) {
+						cout << (i + 1) << " " << (j + 1) << " " << (k + 1);
+						return 0;
+					}
+				}
+			}
+		}
+	}
+
+	cout << "impossible";
+
+	return 0;
+}
