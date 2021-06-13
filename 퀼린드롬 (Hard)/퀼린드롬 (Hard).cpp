9f724d6e72557e1dd9903c81783be88b7e141773
DahamChoi <rorigu32@gmail.com>
2021-06-13 20:07:43 +0900

    ㅠㅠ

--- 퀼린드롬 (Hard)/퀼린드롬 (Hard).cpp
+++ 퀼린드롬 (Hard)/퀼린드롬 (Hard).cpp
@@ -24,100 +24,106 @@ unordered_map<char, char> quillindrom_table = {
     {'8','8'},{'9',' '}
 };
 
-bool is_quillindrom(const string& str, const vector<char>& memory, int last) {
-    int left = 0;
-    while (left <= last) {
-        if (last >= int(str.length())) {
-            if (memory[left] == ' ') {
-                return false;
-            }
+vector<int> getPartialMatch(const string& N) {
+    int m = N.size();
+    vector<int> pi(m, 0);
+
+    int begin = 1, matched = 0;
+    while (begin + matched < m) {
+        if (N[begin + matched] == N[matched]) {
+            ++matched;
+            pi[begin + matched - 1] = matched;
         }
         else {
-            if (memory[left] != str[last]) {
-                if (!isalpha(str[last])) {
-                    return false;
-                }
-
-                if (memory[left] != (isupper(str[last]) ? tolower(str[last]) : toupper(str[last]))) {
-                    return false;
-                }
+            if (matched == 0) {
+                ++begin;
+            }
+            else {
+                begin += matched - pi[matched - 1];
+                matched = pi[matched - 1];
             }
         }
-
-        ++left;
-        --last;
     }
 
-    return true;
+    return pi;
 }
 
-string solution(string str) {
-    vector<char> memory(str.length());
-    for (int i = 0; i < int(str.length()); i++) {
-        if (quillindrom_table[str[i]] == ' ' && isalpha(str[i])) {
-            memory[i] = quillindrom_table[(isupper(str[i]) ? tolower(str[i]) : toupper(str[i]))];
+vector<int> getprefixMatch(const string& s) {
+    vector<int> ret, pi = getPartialMatch(s);
+    int k = s.size();
+    while (k > 0) {
+        ret.push_back(k);
+        k = pi[k - 1];
+    }
+    return ret;
+}
+
+int solution(const string& a, const string& b) {
+    int n = a.size(), m = b.size();
+    vector<int> pi = getPartialMatch(b);
+    int begin = 0, matched = 0;
+    while (begin < n) {
+        if (matched < m && a[begin + matched] == b[matched]) {
+            ++matched;
+            if (begin + matched == n) {
+                return matched;
+            }
         }
         else {
-            memory[i] = quillindrom_table[str[i]];
-        }
-
-        if (memory[i] == ' ') {
-            return "";
+            if (matched == 0) {
+                ++begin;
+            }
+            else {
+                begin += matched - pi[matched - 1];
+                matched = pi[matched - 1];
+            }
         }
     }
 
-    for (int last = str.length() - 1; last <= str.length() * 2; last++) {
-        if (is_quillindrom(str, memory, last)) {
-            for (int i = 0; i < int(memory.size()); i++) {
-                if (isalpha(str[i]) && quillindrom_table[str[i]] != memory[i]) {
-                    str[i] = (isupper(str[i]) ? tolower(str[i]) : toupper(str[i]));
-                }
-            }
-
-            for (int i = last - str.length(); i >= 0; i--) {
-                str.push_back(memory[i]);
-            }
-
-            return str;
-        }
-    }
-
-    return "";
+    return 0;
 }
 
 int main() {
-    ios::sync_with_stdio(false);	cin.tie(NULL);
+    auto func = [&](const char c) {
+        if (quillindrom_table[c] == ' ') {
+            return isupper(c) ? quillindrom_table[tolower(c)] : quillindrom_table[toupper(c)];
+        }
+        return quillindrom_table[c];
+    };
+
     string str; cin >> str;
 
-    string answer_1 = solution(str);
-    reverse(str.begin(), str.end());
-    string answer_2 = solution(str);
-    reverse(answer_2.begin(), answer_2.end());
-
-    if (answer_1.empty() && answer_2.empty()) {
-        assert(false);
-        cout << -1;
-        return 0;
+    string sub_str_1(str.rbegin(), str.rend());
+    transform(sub_str_1.begin(), sub_str_1.end(), sub_str_1.begin(), func);
+    string fix_str_1 = str;
+    for (int i = 0; i < int(fix_str_1.length()); i++) {
+        if (quillindrom_table[fix_str_1[i]] == ' ') {
+            fix_str_1[i] = isupper(fix_str_1[i]) ? tolower(fix_str_1[i]) : toupper(fix_str_1[i]);
+            if (quillindrom_table[fix_str_1[i]] == ' ') {
+                cout << -1;
+                return 0;
+            }
+        }
     }
 
-    if (answer_1.empty()) {
-        assert(false);
-        cout << answer_2;
-        return 0;
-    }
+    int answer_length_1 = str.length() - (solution(fix_str_1, sub_str_1));
 
-    if (answer_2.empty()) {
-        assert(false);
-        cout << answer_1;
-        return 0;
-    }
-    
-    if (answer_1.length() < answer_2.length()) {
-        cout << answer_1;
+    string fix_str_2 = fix_str_1;
+    reverse(fix_str_2.begin(), fix_str_2.end());
+
+    string sub_str_2(fix_str_2.rbegin(), fix_str_2.rend());
+    transform(sub_str_2.begin(), sub_str_2.end(), sub_str_2.begin(), func);
+
+    int answer_length_2 = str.length() - (solution(fix_str_2, sub_str_2));
+
+    if (answer_length_1 < answer_length_2) {
+        cout << fix_str_1;
+        cout << sub_str_1.substr(str.length() - answer_length_1, answer_length_1);
     }
     else {
-        cout << answer_2;
+        cout << sub_str_1.substr(0, answer_length_2);
+        cout << fix_str_1;
     }
-    
+
     return 0;
 }
---
+++ 팰린드롬 만들기/팰린드롬 만들기.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> getPartialMatch(const string& N) {
+    int m = N.size();
+    vector<int> pi(m, 0);
+    
+    int begin = 1, matched = 0;
+    while (begin + matched < m) {
+        if (N[begin + matched] == N[matched]) {
+            ++matched;
+            pi[begin + matched - 1] = matched;
+        }
+        else {
+            if (matched == 0) {
+                ++begin;
+            }
+            else {
+                begin += matched - pi[matched - 1];
+                matched = pi[matched - 1];
+            }
+        }
+    }
+
+    return pi;
+}
+
+vector<int> getprefixMatch(const string& s) {
+    vector<int> ret, pi = getPartialMatch(s);
+    int k = s.size();
+    while (k > 0) {
+        ret.push_back(k);
+        k = pi[k - 1];
+    }
+    return ret;
+}
+
+int solution(const string& a, const string& b) {
+    int n = a.size(), m = b.size();
+    vector<int> pi = getPartialMatch(b);
+    int begin = 0, matched = 0;
+    while (begin < n) {
+        if (matched < m && a[begin + matched] == b[matched]) {
+            ++matched;
+            if (begin + matched == n) {
+                return matched;
+            }
+        }
+        else {
+            if (matched == 0) {
+                ++begin;
+            }
+            else {
+                begin += matched - pi[matched - 1];
+                matched = pi[matched - 1];
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);	cin.tie(NULL);
+
+    int N;      cin >> N;
+    string str; cin >> str;
+    string r_str(str.rbegin(), str.rend());
+
+    cout << (N - solution(str, r_str));
+
+    return 0;
+}
