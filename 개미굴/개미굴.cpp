ec4b1a088df1f855ad31beb15ad09884b6a7c83a
DahamChoi <rorigu32@gmail.com>
2021-06-25 23:27:01 +0900

    거의 최단 경로

---
+++ 개미굴/개미굴.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+vector<string> split_string(string s, string delimiter) {
+    vector<string> result;
+
+    auto pos = 0;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        string token = s.substr(0, pos);
+        result.push_back(token);
+        s.erase(0, pos + delimiter.length());
+    }
+
+    if (!s.empty()) {
+        result.push_back(s);
+    }
+
+    return result;
+}
+
+void loop_printer(map<string, set<string>>& m, const string& key, int depth) {
+    for (auto& it : m[key]) {
+        string tmp(depth * 2, '-');
+        cout << tmp + it << '\n';
+
+        loop_printer(m, key + "\\" + it, depth + 1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);	cin.tie(NULL);
+    int N;	cin >> N;
+
+    map<string, set<string>> m;
+    set<string> root_directory;
+
+    for (int i = 0; i < N; i++) {
+        int K;  cin >> K;
+        vector<string> split_list(K);
+        for (int j = 0; j < K; j++) {
+            cin >> split_list[j];
+        }
+        split_list.push_back("");
+
+        string depth_string;
+        for (int j = 0; j < split_list.size() - 1; j++) {
+            if (j == 0) {
+                root_directory.insert(split_list[j]);
+            }
+
+            if (depth_string.empty()) {
+                depth_string += ((split_list[j]));
+            }
+            else {
+                depth_string += ("\\" + (split_list[j]));
+            }
+
+            if (split_list[j + 1] == "") {
+                m[depth_string].insert("ㅎㅇ");
+                m[depth_string].erase("ㅎㅇ");
+            }
+            else {
+                m[depth_string].insert(split_list[j + 1]);
+            }
+        }
+    }
+
+    for (auto& it : root_directory) {
+        cout << it << '\n';
+        loop_printer(m, it, 1);
+    }
+
+    return 0;
+}
---
+++ 거의 최단 경로/거의 최단 경로.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+
+vector<vector<int>> graph;
+vector<vector<int>> path;
+int N, M, S, D;
+
+int dijkstra() {
+	vector<int> dist(N, 987654321);
+
+	dist[S] = 0;
+	priority_queue<pair<int, int>> pq;
+	pq.push({ 0, S });
+	while (!pq.empty()) {
+		int cost = -pq.top().first;
+		int here = pq.top().second;
+		pq.pop();
+
+		if (dist[here] < cost)	continue;
+
+		for (int there = 0; there < N; there++) {
+			int nextDist = cost + graph[here][there];
+			if (graph[here][there] != -1 && dist[there] > nextDist) {
+				dist[there] = nextDist;
+				path[there].clear();
+				path[there].push_back(here);
+				pq.push({ -nextDist, there });
+			}
+			else if (graph[here][there] != -1 && dist[there] == nextDist) {
+				path[there].push_back(here);
+			}
+		}
+	}
+
+	return dist[D];
+}
+
+void erase_edge() {
+	queue<int> q;
+	q.push(D);
+	vector<bool> visit(N);
+	visit[D] = true;
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+
+		for (auto it : path[cur]) {
+			graph[it][cur] = -1;
+
+			if (!visit[it]) {
+				q.push(it);
+				visit[it] = true;
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);	cin.tie(0);
+
+	while (true) {
+		cin >> N >> M;
+		if (N == 0 && M == 0) {
+			break;
+		}
+
+		cin >> S >> D;
+
+		graph.assign(N, vector<int>(N, -1));
+		path.assign(N, {});
+
+		for (int i = 0; i < M; i++) {
+			int U, V, P;	cin >> U >> V >> P;
+			graph[U][V] = P;
+		}
+
+		dijkstra();
+		erase_edge();
+		int almost_min_distance = dijkstra();
+
+		cout << (almost_min_distance == 987654321 ? -1 : almost_min_distance) << '\n';
+	}
+}
